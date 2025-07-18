f61a378bf6c4039ba723e858bf6f7896f214c89f
MQ <zmingqian@qq.com>
2025-07-18 17:13:05 +0800

    Update config_dev_mirror.sh

--- config_dev_mirror.sh
+++ config_dev_mirror.sh
@@ -1,6 +1,7 @@
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 
 gem sources --add https://gems.ruby-china.com/ --remove https://rubygems.org/
+gem install bundler
 bundle config mirror.https://rubygems.org https://gems.ruby-china.com
 
 npm config set registry https://registry.npmmirror.com
